f523e474367268d4fd97606211032ae53b48b866
2fps <echoweb@126.com>
2019-03-22 21:50:34 +0800

    js都替换为ts文件

--- app.ts
+++ app.ts
@@ -1,9 +1,43 @@
-const Koa = require('koa');
-const app = new Koa();
- 
-app.use(async (ctx: any) => {
- 
-ctx.body = 'Hello World';
+const Koa = require('koa')
+const app = new Koa()
+const views = require('koa-views')
+const json = require('koa-json')
+const onError = require('koa-onerror')
+const bodyparser = require('koa-bodyparser')
+const logger = require('koa-logger')
+
+const index = require('./routes/index')
+
+// error handler
+onError(app);
+
+// middlewares
+app.use(bodyparser({
+    enableTypes:['json', 'form', 'text']
+}))
+app.use(json())
+app.use(logger())
+app.use(require('koa-static')(__dirname + '/public'))
+
+app.use(views(__dirname + '/views', {
+    extension: 'pug'
+}))
+
+// logger
+app.use(async (ctx: any, next: any) => {
+    const start: any = new Date()
+    await next()
+    const stop: any = new Date();
+    const ms: any = stop - start;
+    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+})
+
+// routes
+app.use(index.routes(), index.allowedMethods())
+
+// error-handling
+app.on('error', (err: any, ctx: any) => {
+    console.error('server error', err, ctx)
 });
- 
-app.listen(3000);
+
+module.exports = app
